f460d44cf2bdc826b66af4da0b797e1745635933
wcaceresg <60194135+wcaceresg@users.noreply.github.com>
2022-03-18 11:07:25 -0500

    Update Conection.php

--- app/Components/Core/src/Conection.php
+++ app/Components/Core/src/Conection.php
@@ -5,13 +5,13 @@
  * Date: 17/03/2022
  * Time: 15:05
  */
-class Conection_Central
+class conexionsqlserver
 {        
     public $_connection;
     private static $_instance; //The single instance
-    private  $servidor="192.168.1.200";
+    private  $servidor="192.168.1.0";
     private $usuario="sa";
-    private $password="Villachicken2016";
+    private $password="$password";
     private $bd="Reportes"; 
     // Constructor
     public function __construct()
@@ -27,54 +27,13 @@ class Conection_Central
         return $this->_connection;
     }
 }
-class Conection_Tienda
-{        
-    public $_connection;
-    private static $_instance; //The single instance
-    private  $servidor="192.168.1.222";
-    private $usuario="sa";
-    private $password="Villachicken2016";
-    private $bd="VILLA_DB_ICA"; 
-    // Constructor
-    public function __construct()
-    {
-        //$this->_connection = new mysqli($this->_host, $this->_username,$this->_password, $this->_database);
-        $this->_connection =new PDO("sqlsrv:Server=".$this->servidor .";DataBase=".$this->bd."","".$this->usuario ."","".$this->password."");
-        // Error handling
-        $this->_connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->_connection->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_UTF8);
-    }
-     public function getConnection()
-    {
-        return $this->_connection;
-    }
-}
-class Database
-{        
-    public $_connection;
-    private static $_instance; //The single instance
-    // Constructor
-    public function __construct($_host,$_username,$_password,$_database)
-    {
-        //$this->_connection = new mysqli($this->_host, $this->_username,$this->_password, $this->_database);
-        $this->_connection =new PDO("sqlsrv:Server=".$_host .";DataBase=".$_database ."","".$_username ."","".$_password."");
-        // Error handling
-        $this->_connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->_connection->setAttribute(PDO::SQLSRV_ATTR_ENCODING, PDO::SQLSRV_ENCODING_UTF8);
-    }
-     public function getConnection()
-    {
-        return $this->_connection;
-    }
- 
-}
 class conexionmysql
 {        
     public $_connection;
     private static $_instance; //The single instance
-    private $_dsn = 'mysql:dbname=asteriskdb;host=192.168.1.241';
+    private $_dsn = 'mysql:dbname=dbmyql;host=192.168.1.0';
     private $_user = 'root';
-    private $_password = 'V1ll4PBX2012Maria';
+    private $_password = 'root$';
 
     public static function getInstance()
     {
